649a0869e0e17c7062c75dbcb8cf83bfc7a81a96
carlopp21 <carlitosmancipito21@gmail.com>
2025-08-13 17:31:01 -0500

    CAMBIO A:URL

--- Fortinet/src/main/java/com/Fortinet/Fortinet/Repositorio/usuarioRepositorio.java
+++ Fortinet/src/main/java/com/Fortinet/Fortinet/Repositorio/usuarioRepositorio.java
@@ -6,7 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface usuarioRepositorio extends JpaRepository<Usuario, Long> {
-        List<Usuario> findByCorreoUsuario(String correoUsuario);
-        List<Usuario> findByCargoUsuario(String cargoUsuario);
 
 }
--- Fortinet/src/main/java/com/Fortinet/Fortinet/Servicio/emailService.java
+++ Fortinet/src/main/java/com/Fortinet/Fortinet/Servicio/emailService.java
@@ -7,6 +7,7 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -21,6 +22,7 @@ public class emailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Async
     public void sendHtmlEmailTemplate(Usuario usuario) throws MessagingException {
         Context ctx = new Context();
         // Variables que usa la plantilla (asegúrate que los getters existen en Usuario)
